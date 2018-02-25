e2584c040312f43493e1e93638173a301bb9de70
Andry Ogorodnik <andry.ogorodnik@gmail.com>
2018-02-25 16:12:45 +0000

    Correction for memory mapping

--- gpio/maping.c
+++ gpio/maping.c
@@ -10,36 +10,22 @@
 int mem_fd;
 void *map;
 
-uint32_t *base = (uint32_t *)BCM2837_PERI_BASE;
-uint32_t size = 0x01000000;
+uint32_t base = 0;
+uint32_t size = 0x40960;
 
 int map_pheriferal ()
 {
-  FILE *fp;
-
-  if ((fp = fopen("/proc/device-tree/soc/ranges", "rb"))) {
-    unsigned char buf[4];
-    fseek (fp, 4, SEEK_SET);
-    if (fread(buf, 1, sizeof(buf), fp) == sizeof(buf))
-      base = (uint32_t*)(buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3] << 0);
-    fseek (fp, 8, SEEK_SET);
-    if (fread(buf, 1, sizeof(buf), fp) == sizeof(buf))
-      size = (buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3] << 0);
-    fclose(fp);
-  }
-
   if ((mem_fd = open ("/dev/gpiomem", O_RDWR | O_SYNC) ) < 0) {
     return -1;
   }
 
-  base = 0;
   map = mmap
     ((void *)GPIO_BASE,
     size,
     PROT_READ|PROT_WRITE,
     MAP_SHARED,
     mem_fd,
-    (uint32_t)base);
+    base);
 
   if (map == MAP_FAILED) {
     return -2;
--- gpio/maping.h
+++ gpio/maping.h
@@ -3,9 +3,5 @@
 
 #define BCM2837_PERI_BASE   0x3F000000
 #define GPIO_BASE          (BCM2837_PERI_BASE + 0x200000)
-#define PAGE_SIZE          (4*1024)
-
-extern uint32_t *base;
-extern uint32_t size;
 
 int map_pheriferal ();
