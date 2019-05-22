f479d74318ae88fa0de61cc5c028ad7fbdff7d94
Andry Ogorodnik <andry.ogorodnik@gmail.com>
2019-05-22 19:24:38 +0000

    Corrections for the hexapod constants

--- hexapod/source/hexapod-constants.ads
+++ hexapod/source/hexapod-constants.ads
@@ -39,6 +39,20 @@ package Hexapod.Constants is
    type Leg_Angles_Restrictions is
      array (Leg_Motor_Number) of Angles_Restrictions;
 
+   type Ordinats is (X, Y, Z);
+
+   type Leg_Position is record
+      X : Integer;
+      Y : Integer;
+      Z : Integer;
+   end record;
+   --  Position in milimeters
+
+
+   ---------------
+   -- Constants --
+   ---------------
+
    Legs_PWM_Ranges : constant array (Leg_Number) of Leg_PWM_Ranges :=
      (1 =>
         (1 => (Min => 1250, Max => 2600),
@@ -125,5 +139,26 @@ package Hexapod.Constants is
         (1 => (Min => 0, Max => 180),
          2 => (Min => 0, Max => 180),
          3 => (Min => 0, Max => 180)));
+
+   Legs_Start_Positions : constant
+     array (Leg_Number) of Leg_Position :=
+   --  Positions of legs when they are set in Legs_Start_Angles angles
+   --  Ordinats start points are in the center of bearings of each leg
+   --  Bearings are plased in the bottom of hexapod
+
+     (1 =>
+        (X => -9, Y => 46, Z => -28),
+
+      --  not set
+      2 =>
+        (X => 0, Y => 0, Z => 0),
+      3 =>
+        (X => 0, Y => 0, Z => 0),
+      4 =>
+        (X => 0, Y => 0, Z => 0),
+      5 =>
+        (X => 0, Y => 0, Z => 0),
+      6 =>
+        (X => 0, Y => 0, Z => 0));
 
 end Hexapod.Constants;
