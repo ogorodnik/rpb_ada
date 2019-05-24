e0030cb6f27ae0b551a6c1f39756e1af89407d4e
Andry Ogorodnik <ogorodnik@adacore.com>
2019-05-24 22:20:35 +0300

    Corrections for the hexapod constants

--- hexapod/source/hexapod-constants.ads
+++ hexapod/source/hexapod-constants.ads
@@ -22,6 +22,10 @@ package Hexapod.Constants is
    --   Bdy. 1 - 2     \
    --   --------        \
 
+
+   -- PWM_Range --
+   --  For adjusting motor PWM min/max values if needed
+
    type Motor_PWM_Range is record
       Min : PWM_Value;
       Max : PWM_Value;
@@ -29,25 +33,15 @@ package Hexapod.Constants is
 
    type Leg_PWM_Ranges is array (Leg_Motor_Number) of Motor_PWM_Range;
 
-   type Leg_Start_Angles is array (Leg_Motor_Number) of Angle_Type;
+   -- Angles --
+   --  restrictions in rotation because of hardware limitations
 
-   type Angles_Restrictions is record
+   type Motor_Angles is record
       Min : Angle_Type;
       Max : Angle_Type;
    end record;
 
-   type Leg_Angles_Restrictions is
-     array (Leg_Motor_Number) of Angles_Restrictions;
-
-   type Ordinats is (X, Y, Z);
-
-   type Leg_Position is record
-      X : Integer;
-      Y : Integer;
-      Z : Integer;
-   end record;
-   --  Position in milimeters
-
+   type Leg_Angles is array (Leg_Motor_Number) of Motor_Angles;
 
    ---------------
    -- Constants --
@@ -55,70 +49,37 @@ package Hexapod.Constants is
 
    Legs_PWM_Ranges : constant array (Leg_Number) of Leg_PWM_Ranges :=
      (1 =>
-        (1 => (Min => 1250, Max => 2600),
-         --  1250 is limitation of rotation by hardware,
-         --  use 700 if Legs_Angles_Restrictions is in use
-         2 => (Min => 700,  Max => 2400),
-         3 => (Min => 650,  Max => 2400)),
-
-      --  not set
+        (1 => (Min => 700, Max => 2400),
+         2 => (Min => 700, Max => 2400),
+         3 => (Min => 700, Max => 2400)),
       2 =>
-        (1 => (Min => 0, Max => 0),
-         2 => (Min => 0, Max => 0),
-         3 => (Min => 0, Max => 0)),
+        (1 => (Min => 700, Max => 2400),
+         2 => (Min => 700, Max => 2400),
+         3 => (Min => 700, Max => 2400)),
       3 =>
-        (1 => (Min => 0, Max => 0),
-         2 => (Min => 0, Max => 0),
-         3 => (Min => 0, Max => 0)),
+        (1 => (Min => 700, Max => 2400),
+         2 => (Min => 700, Max => 2400),
+         3 => (Min => 700, Max => 2400)),
       4 =>
-        (1 => (Min => 0, Max => 0),
-         2 => (Min => 0, Max => 0),
-         3 => (Min => 0, Max => 0)),
+        (1 => (Min => 700, Max => 2400),
+         2 => (Min => 700, Max => 2400),
+         3 => (Min => 700, Max => 2400)),
       5 =>
-        (1 => (Min => 0, Max => 0),
-         2 => (Min => 0, Max => 0),
-         3 => (Min => 0, Max => 0)),
+        (1 => (Min => 700, Max => 2400),
+         2 => (Min => 700, Max => 2400),
+         3 => (Min => 700, Max => 2400)),
       6 =>
-        (1 => (Min => 0, Max => 0),
-         2 => (Min => 0, Max => 0),
-         3 => (Min => 0, Max => 0)));
+        (1 => (Min => 700, Max => 2400),
+         2 => (Min => 700, Max => 2400),
+         3 => (Min => 700, Max => 2400)));
 
-   Legs_Start_Angles : constant array (Leg_Number) of Leg_Start_Angles :=
+   Legs_Angles : constant
+     array (Leg_Number) of Leg_Angles :=
      (1 =>
-        (1 => 180,
-         2 => 0,
-         3 => 0),
-
-      --  not set
-      2 =>
-        (1 => 90,
-         2 => 90,
-         3 => 90),
-      3 =>
-        (1 => 90,
-         2 => 90,
-         3 => 90),
-      4 =>
-        (1 => 90,
-         2 => 90,
-         3 => 90),
-      5 =>
-        (1 => 90,
-         2 => 90,
-         3 => 90),
-      6 =>
-        (1 => 90,
-         2 => 90,
-         3 => 90));
-
-   Legs_Angles_Restrictions : constant
-     array (Leg_Number) of Leg_Angles_Restrictions :=
-     (1 =>
-        (1 => (Min => 53, Max => 180),
+        (1 => (Min => 60, Max => 180),
          2 => (Min => 0,  Max => 180),
          3 => (Min => 0,  Max => 180)),
 
-      --  not set
       2 =>
         (1 => (Min => 0, Max => 180),
          2 => (Min => 0, Max => 180),
@@ -136,29 +97,8 @@ package Hexapod.Constants is
          2 => (Min => 0, Max => 180),
          3 => (Min => 0, Max => 180)),
       6 =>
-        (1 => (Min => 0, Max => 180),
+        (1 => (Min => 60, Max => 180),
          2 => (Min => 0, Max => 180),
          3 => (Min => 0, Max => 180)));
-
-   Legs_Start_Positions : constant
-     array (Leg_Number) of Leg_Position :=
-   --  Positions of legs when they are set in Legs_Start_Angles angles
-   --  Ordinats start points are in the center of bearings of each leg
-   --  Bearings are plased in the bottom of hexapod
-
-     (1 =>
-        (X => -9, Y => 46, Z => -28),
-
-      --  not set
-      2 =>
-        (X => 0, Y => 0, Z => 0),
-      3 =>
-        (X => 0, Y => 0, Z => 0),
-      4 =>
-        (X => 0, Y => 0, Z => 0),
-      5 =>
-        (X => 0, Y => 0, Z => 0),
-      6 =>
-        (X => 0, Y => 0, Z => 0));
 
 end Hexapod.Constants;
